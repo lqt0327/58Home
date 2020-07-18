50f7cc10a123cda87e2d859dc8aeb92318b459da
wuyongxugogogo <474643974@qq.com>
2020-07-18 17:34:54 +0800

    serve header

--- src/pages/home/serve/index.jsx
+++ src/pages/home/serve/index.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import '../../../assets/css/home/serve/index.css';
+import searchImg from '../../../assets/images/home/serve/search.png';
+import addImg from '../../../assets/images/home/serve/add.png';
 
-export default function Serve(){
-    return(
+export default function Serve() {
+    return (
         <div className="serve">
-            <div className="serveHeader"></div>
+            <div className="serveHeader">
+                <div className="serveSearchBox">
+                    <div className="searchImgBox">
+                        <img src={searchImg} alt="" />
+                    </div>
+                    <div className="searchInputBox">
+                        <input type="text" placeholder="擦玻璃" />
+                    </div>
+
+                </div>
+                <div className="serveTopRight">
+                    <img src={addImg} alt=""/>
+                </div>
+            </div>
+            <div className="serveWrapper">
+                <div className="serveLeft"></div>
+                <div className="serveRight"></div>
+            </div>
         </div>
     )
 }
