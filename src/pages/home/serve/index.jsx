7edac80eb6d3e1c5594024bc92a3512eedadf90d
wuyongxugogogo <474643974@qq.com>
2020-07-20 21:03:34 +0800

    bscroll

--- src/pages/home/serve/index.jsx
+++ src/pages/home/serve/index.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import BScroll from 'better-scroll';
+import data from './serve.json';
 import '../../../assets/css/home/serve/index.css';
 import searchImg from '../../../assets/images/home/serve/search.png';
 import addImg from '../../../assets/images/home/serve/add.png';
+import { useState } from 'react';
 
 export default function Serve() {
+    let [isActive,setActive] = useState(1)
+    console.log(data)
+    useEffect(() => {
+        const scroll = new BScroll('.serveRight', {
+            scrollY: true,
+            click: false,
+            scrollX: false,
+            snap: {  // 滑块切换的一些配置
+                threshold: 0.5,  // 滑动切换到超过一半时切换到下一屏
+                // stepY: calc(100vh - 2.2rem)  // 纵向切换距离为窗口高度
+            }
+        })
+    }, [])
+    let scrollToAnthor = (id) => {
+        document.getElementById(id).scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+    }
+    
     return (
+
         <div className="serve">
             <div className="serveHeader">
                 <div className="serveSearchBox">
@@ -17,12 +38,39 @@ export default function Serve() {
 
                 </div>
                 <div className="serveTopRight">
-                    <img src={addImg} alt=""/>
+                    <img src={addImg} alt="" />
                 </div>
             </div>
             <div className="serveWrapper">
-                <div className="serveLeft"></div>
-                <div className="serveRight"></div>
+                <div className="serveLeft">
+                    <ul>
+                        {
+                            data.map((item, i) => {
+                                return (
+                                    <li className={`${isActive===i+1?'active':''}`} onClick={() => {
+                                        setActive(isActive = i+1)
+                                        scrollToAnthor(`${i+1}`)
+                                    }} key={i}>
+                                        {item.name}
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+                </div>
+                <div className="serveRight">
+                    <ul className="pagesContent">
+                        {
+                            data.map((item, i) => {
+                                return (
+                                    <li id={i+1} key={i}>
+                                        {item.name}
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+                </div>
             </div>
         </div>
     )
