108918fd115f1376b41d155d82752eb029625fde
wuyongxugogogo <474643974@qq.com>
2020-07-21 21:07:04 +0800

    左右同步滚动待完善

--- src/pages/home/serve/index.jsx
+++ src/pages/home/serve/index.jsx
@@ -1,31 +1,34 @@
-import React, { useEffect } from 'react';
+import React, { useEffect,useState } from 'react';
 import BScroll from 'better-scroll';
 import data from './serve.json';
 import '../../../assets/css/home/serve/index.css';
 import searchImg from '../../../assets/images/home/serve/search.png';
 import addImg from '../../../assets/images/home/serve/add.png';
-import { useState } from 'react';
+import LazyLoad from 'react-lazyload';
 
 export default function Serve() {
-    let [isActive,setActive] = useState(1)
-    console.log(data)
+    let [isActive, setActive] = useState(1)
+    // console.log(data)
     useEffect(() => {
-        const scroll = new BScroll('.serveRight', {
+        let scroll = new BScroll('.serveRight', {
             scrollY: true,
-            click: false,
+            click: true,
             scrollX: false,
             snap: {  // 滑块切换的一些配置
                 threshold: 0.5,  // 滑动切换到超过一半时切换到下一屏
                 // stepY: calc(100vh - 2.2rem)  // 纵向切换距离为窗口高度
             }
         })
+        function onScroll(pos) {
+            console.log(`Now position is x: ${pos.x}, y: ${pos.y}`)
+          }
+        scroll.on('scroll', onScroll)
     }, [])
     let scrollToAnthor = (id) => {
         document.getElementById(id).scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
     }
-    
+    // console.log(data[0].content)
     return (
-
         <div className="serve">
             <div className="serveHeader">
                 <div className="serveSearchBox">
@@ -47,9 +50,9 @@ export default function Serve() {
                         {
                             data.map((item, i) => {
                                 return (
-                                    <li className={`${isActive===i+1?'active':''}`} onClick={() => {
-                                        setActive(isActive = i+1)
-                                        scrollToAnthor(`${i+1}`)
+                                    <li className={`${isActive === i + 1 ? 'active' : ''}`} onClick={() => {
+                                        setActive(isActive = i + 1)
+                                        scrollToAnthor(`${i + 1}`)
                                     }} key={i}>
                                         {item.name}
                                     </li>
@@ -63,8 +66,27 @@ export default function Serve() {
                         {
                             data.map((item, i) => {
                                 return (
-                                    <li id={i+1} key={i}>
-                                        {item.name}
+                                    <li id={i + 1} key={i}>
+                                        <div className="serveContent">
+                                            <div className="topImg">
+                                                <img src={item.content.topImg} alt="" />
+                                            </div>
+                                            <span className="itemTitle">{item.content.title}</span>
+                                            <div className="serveItem">
+                                                {item.content.serveItem.map((sItem, j) => {
+                                                    return (
+                                                        <div className="sItemWrapper" key={j}>
+                                                            <div className="sItemImg">
+                                                                {/* <LazyLoad> */}
+                                                                <img src={sItem.itemImg} alt="" />
+                                                                {/* </LazyLoad> */}
+                                                            </div>
+                                                            <span className="sItemName">{sItem.itemName}</span>
+                                                        </div>
+                                                    )
+                                                })}
+                                            </div>
+                                        </div>
                                     </li>
                                 )
                             })
