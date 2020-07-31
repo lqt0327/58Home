f468101b4d2b37ebfaad11f11f42c89122b51945
Charlie2244 <lhs2244@163.com>
2020-07-31 08:35:14 +0800

    r

--- src/router.js
+++ src/router.js
@@ -4,6 +4,8 @@ import config from './assets/js/conf/config.js';
 const HomeComponent = lazy(() => import('./pages/home/home/index'));
 const Detail = lazy(() => import('./pages/home/serve/detail.jsx'));
 const newAdress = lazy(() => import('./pages/user/adress/newAdress.jsx'))
+const EditAdress = lazy(() => import('./pages/user/adress/editAdress.jsx'))
+const AdressList = lazy(() => import('./pages/user/adress/adressList'))
 const myOrder = lazy(()=>import('./pages/user/myOrder/index.jsx'))
 const OrderDetails = lazy(()=> import('./pages/user/myOrder/orderDetails'));
 
@@ -17,6 +19,8 @@ export default function RouterComponent() {
                             <Route path={config.path + "home"} component={HomeComponent} ></Route>
                             <Route path='/serve/detail' component={Detail}/>
                             <Route path='/user/newadress' component={newAdress}/>
+                            <Route path='/user/adressList' component={AdressList}/>
+                            <Route path='/user/editadress/:id' component={EditAdress}/>
                             <Route path='/user/myorder' component={myOrder}/>
                             <Route path='/order/details' component={OrderDetails}/>
                             <Redirect to={config.path + "home/index"}></Redirect>
